bbd0fb19d6b3d61c670d20f9637769534a5f7b7b
Alexander Hewitson <alexander.hewitson@sky.uk>
2022-09-08 13:45:12 +0100

    feat(): update configuration

--- apps/occ-spike/src/app/app.component.ts
+++ apps/occ-spike/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'occ-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'occ-spike';
+
+  ngOnInit() {
+    console.log('Test');
+  }
 }
